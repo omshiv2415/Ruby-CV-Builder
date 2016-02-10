be28183f35cb93dcfec3c4182b44a8e228f7c769
Viral <omshiv2415@yahoo.co.uk>
2016-02-10 21:29:44 +0000

    slideshow

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'devise-bootstrap-views'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
-
+gem 'slideshow', '~> 2.4', '>= 2.4.1'
 gem 'bootstrap-sass', '~> 3.3.5'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  helper_method :current_user, :logged_in?
+
+	def logged_in?
+	!!current_user
+	end
+
+	 def require_user
+	  if !logged_in?
+	  flash[:danger] = "You must be logged in to perform that action"
+    redirect_to root_path
+   end
+ end
 end
