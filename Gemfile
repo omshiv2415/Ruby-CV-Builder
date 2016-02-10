0cc2150bd5c6d07cc5cc1b0df60d1aae1bedc59a
Viral <omshiv2415@yahoo.co.uk>
2016-02-10 16:11:43 +0000

    initial

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
-
+gem 'devise'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5.1'
 # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
-
+  gem 'devise'
+  gem 'twitter-bootstrap-rails'
+  gem 'devise-bootstrap-views'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
