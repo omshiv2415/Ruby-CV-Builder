a030c5eb16659859885e160b5b6bccb44585da88
Viral <omshiv2415@yahoo.co.uk>
2016-02-11 10:03:49 +0000

    useradded

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'abcbankmsc.herokuapp.com', :protocol => 'https'}
+  config.action_mailer.default_url_options = { :host => 'rubycvbuilders.herokuapp.com', :protocol => 'https'}
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
