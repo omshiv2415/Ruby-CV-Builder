d0aa1d7f99fa28a10b8bded30c2777fa2c16e570
Viral <omshiv2415@yahoo.co.uk>
2016-04-12 11:26:40 +0000

    phase5

--- Gemfile
+++ Gemfile
@@ -60,6 +60,7 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
   gem "rails-erd"
+  #rake diagram:all
 	gem 'railroady'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
--- db/migrate/20160407210919_add_foreign_key_to_skills.rb
+++ db/migrate/20160407210919_add_foreign_key_to_skills.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyToSkills < ActiveRecord::Migration
   def change
-    add_reference :experiences, :person, index: true, foreign_key: true
+
     add_reference :jobpreferences, :person, index: true, foreign_key: true
     add_reference :skills, :person, index: true, foreign_key: true
     add_reference :educatioal_qualifications, :person, index: true, foreign_key: true
