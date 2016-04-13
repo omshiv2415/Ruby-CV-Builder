dcefae41b43f8bf8ef083bb05d0ed1f07ce307f1
Viral <omshiv2415@yahoo.co.uk>
2016-04-13 01:06:12 +0000

    phase5

--- db/migrate/20160413010132_add_foreign_key.rb
+++ db/migrate/20160413010132_add_foreign_key.rb
@@ -1,5 +1,5 @@
 class AddForeignKey < ActiveRecord::Migration
   def change
-     add_reference :skills, :person, index: true, foreign_key: true
+     add_reference :skills, :user, index: true, foreign_key: true
   end
 end
