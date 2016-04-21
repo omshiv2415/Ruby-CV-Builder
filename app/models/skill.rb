8ada964d11887b951e11616827f9f6565d7c3765
Viral <omshiv2415@yahoo.co.uk>
2016-04-21 14:51:10 +0000

    Second Draft

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,6 +5,8 @@ class Skill < ActiveRecord::Base
 
   validates :skillType, presence: true, length: { minimum: 1, maximum: 15 }
   validates :skillName, presence: true, length: { minimum: 1, maximum: 15 }
+  validates :skillName, uniqueness: { case_sensitive: false }
+  validates_uniqueness_of :skillName, scope: :user_id
   validates :skillLevel, presence: true, length: { minimum: 1, maximum: 75 }
 
   before_save :user_setup
