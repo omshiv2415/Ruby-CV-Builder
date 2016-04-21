7fec808a0f06b7fa5a347cfc4f481eb4e895b250
Viral <omshiv2415@yahoo.co.uk>
2016-04-21 19:46:51 +0000

    initial

--- app/models/person.rb
+++ app/models/person.rb
@@ -51,7 +51,7 @@ class Person < ActiveRecord::Base
 
   def self.search(search_skill)
     if search_skill
-      joins(:skill).where('skills.skillname ilike ?', "%#{search_skill}%")
+      joins(:skill).where('skills.skillName like ?', "%#{search_skill}%")
     else
       find(:all)
     end
